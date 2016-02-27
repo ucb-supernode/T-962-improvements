17b7cd32be572cddd8d0d82bd0e549878442e4c9
Calvin Li <calvin.li.lt@gmail.com>
2016-02-27 00:13:08 -0800

    modifications to live debug chart script for OS X
    Should still work in Linux/Windows with correct TTY list.
    
    I accidentally did a :retab in vim. Sorry.
    
    Ideally for OS X you should use MacOSX
    matplotlib backend, but it has an annoying
    bug where doing a draw steals focus to the figure.

--- serial-control.py
+++ serial-control.py
@@ -12,16 +12,20 @@
 
 import csv
 import datetime
-import matplotlib.pyplot as plt
-import matplotlib.gridspec as gridspec
 import serial
 import sys
 from time import time
 
+import matplotlib
+matplotlib.use('WXAgg')
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+
 # settings
 #
 FIELD_NAMES = 'Time,Temp0,Temp1,Temp2,Temp3,Set,Actual,Heat,Fan,ColdJ,Mode'
-TTYs = ('/dev/ttyUSB0', '/dev/ttyUSB1', '/dev/ttyUSB2')
+TTYs = (#'/dev/ttyUSB0', '/dev/ttyUSB1', '/dev/ttyUSB2',
+        '/dev/tty.PL2303-00002014', '/dev/tty.PL2303-00001014')
 BAUD_RATE = 115200
 
 logdir = 'logs/'
@@ -33,235 +37,247 @@ MAX_Y_pwm = 260
 # end of settings
 
 def timestamp(dt=None):
-	if dt is None:
-		dt = datetime.datetime.now()
+    if dt is None:
+        dt = datetime.datetime.now()
 
-	return dt.strftime('%Y-%m-%d-%H%M%S')
+    return dt.strftime('%Y-%m-%d-%H%M%S')
 
 
 def logname(filetype, profile):
-	return '%s%s-%s.%s' % (
-		logdir,
-		timestamp(),
-		profile.replace(' ', '_').replace('/', '_'),
-		filetype
-	)
+    return '%s%s-%s.%s' % (
+        logdir,
+        timestamp(),
+        profile.replace(' ', '_').replace('/', '_'),
+        filetype
+    )
 
 
 def get_tty():
-	for devname in TTYs:
-		try:
-			port = serial.Serial(devname, baudrate=BAUD_RATE)
-			print 'Using serial port %s' % port.name
-			return port
+    for devname in TTYs:
+        try:
+            port = serial.Serial(devname, baudrate=BAUD_RATE)
+            print 'Using serial port %s' % port.name
+            return port
 
-		except:
-			print 'Tried serial port %s, but failed.' % str(devname)
-			pass
+        except:
+            print 'Tried serial port %s, but failed.' % str(devname)
+            pass
 
-	return None
+    return None
 
 
 class Line(object):
-	def __init__(self, axis, key, label=None):
-		self.xvalues = []
-		self.yvalues = []
+    def __init__(self, axis, key, label=None):
+        self.xvalues = []
+        self.yvalues = []
 
-		self._key = key
-		self._line, = axis.plot(self.xvalues, self.yvalues, label=label or key)
+        self._key = key
+        self._line, = axis.plot(self.xvalues, self.yvalues, label=label or key)
 
-	def add(self, log):
-		self.xvalues.append(log['Time'])
-		self.yvalues.append(log[self._key])
+    def add(self, log):
+        self.xvalues.append(log['Time'])
+        self.yvalues.append(log[self._key])
 
-		self.update()
+        self.update()
 
-	def update(self):
-		self._line.set_data(self.xvalues, self.yvalues)
+    def update(self):
+        self._line.set_data(self.xvalues, self.yvalues)
 
-	def clear(self):
-		self.xvalues = []
-		self.yvalues = []
+    def clear(self):
+        self.xvalues = []
+        self.yvalues = []
 
-		self.update()
+        self.update()
 
 class Log(object):
-	profile = ''
-	last_action = None
+    profile = ''
+    last_action = None
 
-	def __init__(self):
-		self.init_plot()
-		self.clear_logs()
+    def __init__(self):
+        self.init_plot()
+        self.clear_logs()
 
-	def clear_logs(self):
-		self.raw_log = []
-		map(Line.clear, self.lines)
-		self.mode = ''
+    def clear_logs(self):
+        self.raw_log = []
+        map(Line.clear, self.lines)
+        self.mode = ''
 
-	def init_plot(self):
-		plt.ion()
+    def init_plot(self):
+        plt.ion()
 
-		gs = gridspec.GridSpec(2, 1, height_ratios=(4, 1))
-		fig = plt.figure(figsize=(14, 10))
+        gs = gridspec.GridSpec(2, 1, height_ratios=(4, 1))
+        fig = plt.figure(figsize=(7, 5))
 
-		axis_upper = fig.add_subplot(gs[0])
-		axis_lower = fig.add_subplot(gs[1])
-		plt.subplots_adjust(hspace=0.05, top=0.95, bottom=0.05, left=0.05, right=0.95)
+        axis_upper = fig.add_subplot(gs[0])
+        axis_lower = fig.add_subplot(gs[1], sharex=axis_upper)
+        plt.subplots_adjust(hspace=0.05, top=0.95, bottom=0.10, left=0.10, right=0.95)
 
-		# setup axis for upper graph (temperature values)
-		axis_upper.set_ylabel(u'Temperature [°C]')
-		axis_upper.set_xlim(0, MAX_X)
-		axis_upper.set_xticklabels([])
-		axis_upper.set_ylim(0, MAX_Y_temperature)
+        # setup axis for upper graph (temperature values)
+        axis_upper.set_ylabel(u'Temperature [°C]')
+        axis_upper.set_xlim(-5, MAX_X)
+        axis_upper.set_ylim(-5, MAX_Y_temperature)
+        plt.setp( axis_upper.get_xticklabels(), visible=False)
 
-		# setup axis for lower graph (PWM values)
-		axis_lower.set_xlim(0, MAX_X)
-		axis_lower.set_ylim(0, MAX_Y_pwm)
-		axis_lower.set_ylabel('PWM value')
-		axis_lower.set_xlabel('Time [s]')
+        # setup axis for lower graph (PWM values)
+        axis_lower.set_xlim(-5, MAX_X)
+        axis_lower.set_ylim(-5, MAX_Y_pwm)
+        axis_lower.set_ylabel('PWM value')
+        axis_lower.set_xlabel('Time [s]')
 
-		# select values to be plotted
-		self.lines = [
-			Line(axis_upper, 'Actual'),
-			Line(axis_upper, 'Temp0'),
-			Line(axis_upper, 'Temp1'),
-			Line(axis_upper, 'Set', u'Setpoint'),
-			Line(axis_upper, 'ColdJ', u'Coldjunction'),
-		#	Line(axis_upper, 'Temp2'),
-		#	Line(axis_upper, 'Temp3'),
+        # select values to be plotted
+        self.lines = [
+            Line(axis_upper, 'Actual'),
+            Line(axis_upper, 'Temp0'),
+            Line(axis_upper, 'Temp1'),
+            Line(axis_upper, 'Set', u'Setpoint'),
+            Line(axis_upper, 'ColdJ', u'Coldjunction'),
+        #   Line(axis_upper, 'Temp2'),
+        #   Line(axis_upper, 'Temp3'),
 
-			Line(axis_lower, 'Fan'),
-			Line(axis_lower, 'Heat', 'Heater')
-		]
+            Line(axis_lower, 'Fan'),
+            Line(axis_lower, 'Heat', 'Heater')
+        ]
 
-		axis_upper.legend()
-		axis_lower.legend()
-		plt.draw()
+        axis_upper.legend()
+        axis_lower.legend()
+        plt.draw()
+        plt.pause(0.1)
 
-		self.axis_upper = axis_upper
-		self.axis_lower = axis_lower
+        self.axis_upper = axis_upper
+        self.axis_lower = axis_lower
 
-	def save_logfiles(self):
-		print 'Saved log in %s ' % logname('csv', self.profile)
-		plt.savefig(logname('png', self.profile))
-		plt.savefig(logname('pdf', self.profile))
 
-		with open(logname('csv', self.profile), 'w+') as csvout:
-			writer = csv.DictWriter(csvout, FIELD_NAMES.split(','))
-			writer.writeheader()
+    def save_logfiles(self):
+        print 'Saved log in %s ' % logname('csv', self.profile)
+        plt.savefig(logname('png', self.profile))
+        plt.savefig(logname('pdf', self.profile))
 
-			for l in self.raw_log:
-				writer.writerow(l)
+        with open(logname('csv', self.profile), 'w+') as csvout:
+            writer = csv.DictWriter(csvout, FIELD_NAMES.split(','))
+            writer.writeheader()
 
-	def parse(self, line):
-		values = map(str.strip, line.split(','))
-		# Convert all values to float, except the mode
-		values = map(float, values[0:-1]) + [values[-1], ]
+            for l in self.raw_log:
+                writer.writerow(l)
 
-		fields = FIELD_NAMES.split(',')
-		if len(values) != len(fields):
-			raise ValueError('Expected %d fields, found %d' % (len(fields), len(values)))
+    def parse(self, line):
+        values = map(str.strip, line.split(','))
+        # Convert all values to float, except the mode
+        values = map(float, values[0:-1]) + [values[-1], ]
 
-		return dict(zip(fields, values))
+        fields = FIELD_NAMES.split(',')
+        if len(values) != len(fields):
+            raise ValueError('Expected %d fields, found %d' % (len(fields), len(values)))
 
-	def process_log(self, logline):
-		# ignore 'comments'
-		if logline.startswith('#'):
-			print logline
-			return
+        return dict(zip(fields, values))
 
-		# parse Profile name
-		if logline.startswith('Starting reflow with profile: '):
-			self.profile = logline[30:].strip()
-			return
+    def process_log(self, logline):
+        # ignore 'comments'
+        if logline.startswith('#'):
+            print logline
+            return
 
-		if logline.startswith('Selected profile'):
-			self.profile = logline[20:].strip()
-			return
+        # parse Profile name
+        if logline.startswith('Starting reflow with profile: '):
+            self.profile = logline[30:].strip()
+            return
 
-		try:
-			log = self.parse(logline)
-		except ValueError, e:
-			if len(logline) > 0:
-				print '!!', logline
-			return
+        if logline.startswith('Selected profile'):
+            self.profile = logline[20:].strip()
+            return
 
-		if 'Mode' in log:
-			# clean up log before starting reflow
-			if self.mode == 'STANDBY' and log['Mode'] in ('BAKE', 'REFLOW'):
-				self.clear_logs()
+        try:
+            log = self.parse(logline)
+            print log
+        except ValueError, e:
+            if len(logline) > 0:
+                print '!!', logline
+            return
 
-			# save png graph an csv file when bake or reflow ends.
-			if self.mode in ('BAKE', 'REFLOW') and log['Mode'] == 'STANDBY':
-				self.save_logfiles()
+        if 'Mode' in log:
+            # clean up log before starting reflow
+            if self.mode == 'STANDBY' and log['Mode'] in ('BAKE', 'REFLOW'):
+                self.clear_logs()
 
-			self.mode = log['Mode']
-			if log['Mode'] == 'BAKE':
-				self.profile = 'bake'
+            # save png graph an csv file when bake or reflow ends.
+            if self.mode in ('BAKE', 'REFLOW') and log['Mode'] == 'STANDBY':
+                self.save_logfiles()
 
-			if log['Mode'] in ('REFLOW', 'BAKE'):
-				self.last_action = time()
-			self.axis_upper.set_title('Profile: %s Mode: %s ' % (self.profile, self.mode))
+            self.mode = log['Mode']
+            if log['Mode'] == 'BAKE':
+                self.profile = 'bake'
 
-		if 'Time' in log and log['Time'] != 0.0:
-			if 'Actual' not in log:
-				return
+            if log['Mode'] in ('REFLOW', 'BAKE'):
+                self.last_action = time()
+            self.axis_upper.set_title('Profile: %s Mode: %s ' % (self.profile, self.mode))
 
-			# update all lines
-			map(lambda x: x.add(log), self.lines)
-			self.raw_log.append(log)
+        if 'Time' in log and log['Time'] != 0.0:
+            if 'Actual' not in log:
+                return
 
-		# update view
-		plt.draw()
+            # update all lines
+            map(lambda x: x.add(log), self.lines)
+            self.raw_log.append(log)
 
-	def isdone(self):
-		return (
-			self.last_action is not None and
-			time() - self.last_action > 5
-		)
+        # update view
+        plt.draw()
+        plt.pause(0.001)
+
+    def isdone(self):
+        return (
+            self.last_action is not None and
+            time() - self.last_action > 5
+        )
 
 
 def loop_all_profiles(num_profiles=6):
-	log = Log()
+    log = Log()
 
-	with get_tty() as port:
-		profile = 0
-		def select_profile(profile):
-			port.write('stop\n')
-			port.write('select profile %d\n' % profile)
-			port.write('reflow\n')
+    with get_tty() as port:
+        profile = 0
+        def select_profile(profile):
+            port.write('stop\n')
+            port.write('select profile %d\n' % profile)
+            port.write('reflow\n')
 
-		select_profile(profile)
+        select_profile(profile)
 
-		while True:
-			logline = port.readline().strip()
+        while True:
+            logline = port.readline().strip()
 
-			if log.isdone():
-				log.last_action = None
-				profile += 1
-				if profile > 6:
-					print 'Done.'
-					sys.exit()
-				select_profile(profile)
+            if log.isdone():
+                log.last_action = None
+                profile += 1
+                if profile > 6:
+                    print 'Done.'
+                    sys.exit()
+                select_profile(profile)
 
-			log.process_log(logline)
+            log.process_log(logline)
 
 def logging_only():
-	log = Log()
+    log = Log()
 
-	with get_tty() as port:
-		while True:
-			log.process_log(port.readline().strip())
+    port = get_tty()
+    if not port:
+        return
+    try:
+        while True:
+            serial_line = port.readline().strip()
+            log.process_log(serial_line)
+    except Exception:
+        sys.exit(0)
+    finally:
+        port.close()
 
 if __name__ == '__main__':
-	action = sys.argv[1] if len(sys.argv) > 1 else 'log'
+    action = sys.argv[1] if len(sys.argv) > 1 else 'log'
 
-	if action == 'log':
-		print 'Logging reflow sessions...'
-		logging_only()
+    if action == 'log':
+        print 'Logging reflow sessions...'
+        logging_only()
 
-	elif action == 'test':
-		print 'Looping over all profiles'
-		loop_all_profiles()
-	else:
-		print 'Unknown action', action
+    elif action == 'test':
+        print 'Looping over all profiles'
+        loop_all_profiles()
+    else:
+        print 'Unknown action', action
